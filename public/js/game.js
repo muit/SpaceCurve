eeadfe020b7d3bc38e74575a7c1f54dad28eec0e
muit <miguel_3c@hotmail.com>
2014-08-27 11:35:37 +0200

    Initial Structure

---
+++ public/js/game.js
@@ -0,0 +1,64 @@
+//*************************************************************************
+// Game Class
+//************************
+function Game(){
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth, window.innerHeight, 0.1, 1000);
+    var renderer = new THREE.WebGLRenderer();
+
+    renderer.setClearColor(0x555555);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    $("#canvas").append(renderer.domElement);
+};
+
+
+//*************************************************************************
+// Object Class
+//************************
+Game.Object = function(){}
+
+//************************
+//Object Types
+//************************
+Game.Object.Speed = function(){}
+Game.Object.Speed.prototype = new Game.Object();
+Game.Object.Speed.prototype.constructor = Game.Object.Speed;
+
+
+
+//*************************************************************************
+// Entity Class
+//************************
+Game.Entity = function(){}
+Game.Entity.prototype.say = function(){
+    console.log("Hola!");
+};
+
+
+//************************
+// IA Class
+//************************
+Game.IAEntity = function(){
+    //Entity.call(this);
+}
+Game.IAEntity.prototype = new Game.Entity();
+Game.IAEntity.prototype.constructor = Game.IAEntity;
+
+Game.IAEntity.prototype.say = function(){
+    console.log("Hola IA!");
+};
+
+
+//************************
+// Player Class
+//************************
+Game.Player = function(){
+    //Entity.call(this);
+}
+Game.Player.prototype = new Game.Entity();
+Game.Player.prototype.constructor = Game.Player;
+
+Game.Player.prototype.say = function(){
+    console.log("Hola Player!");
+};
---
+++ public/js/main.js
@@ -0,0 +1,3 @@
+$(function(){
+    game = new Game();
+});
