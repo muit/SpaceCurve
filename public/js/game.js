9593bca6dbd7a541c80801b32708974ff6ee12e1
muit <miguel_3c@hotmail.com>
2014-08-29 18:10:08 +0200

    Render method

--- public/js/game.js
+++ public/js/game.js
@@ -2,15 +2,25 @@
 // Game Class
 //************************
 function Game(){
-    var scene = new THREE.Scene();
+    this.done = false;
+    this.scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(45, window.innerWidth, window.innerHeight, 0.1, 1000);
-    var renderer = new THREE.WebGLRenderer();
+    this.renderer = new THREE.WebGLRenderer();
 
     renderer.setClearColor(0x555555);
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     $("#canvas").append(renderer.domElement);
+
+    this.render();
 };
+
+Game.prototype.render = function(){
+    this.renderer.render(this.scene, this.camera); 
+    if(!done)
+        requestAnimationFrame(this.render);
+}
+
 
 
 //*************************************************************************
