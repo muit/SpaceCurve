56f48e27766c1069ba284e6513fd4bab34e93103
muit <miguel_3c@hotmail.com>
2014-08-29 22:45:17 +0200

    Added Util Methods

--- src/util.js
+++ src/util.js
@@ -9,3 +9,37 @@ String.prototype.endsWith = function(suffix) {
 String.prototype.contains = function(str){
     return this.indexOf(str) != -1;
 };
+
+//*******************************
+// Get Keys from a hash
+//*******************************
+Object.prototype.keys = function(){
+    var ret=[],p;
+    for(p in this) if(Object.prototype.hasOwnProperty.call(this,p)) ret.push(p);
+    return ret;
+}
+
+//*******************************
+// Hash helpers
+//*******************************
+Object.prototype.keys = function(){
+    var ret=[],p;
+    for(p in this) if(Object.prototype.hasOwnProperty.call(this,p)) ret.push(p);
+    return ret;
+}
+
+Object.prototype.key = function(o){
+    var ret=[],p;
+    for(p in this) if(this[p] == o) ret.push(p);
+    return ret;
+}
+
+//*******************************
+// Array Helpers
+//*******************************
+Object.prototype.first = function(){ return this[0]; }
+Object.prototype.last = function(){ return this[this.length-1]; }
+Object.prototype.remove = function(o){ 
+    var i = this.indexOf(o);
+    return (i >= 0)? this.splice(i, 1)[0] : undefined;
+}
