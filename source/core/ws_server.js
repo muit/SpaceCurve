2e6ae540699970702a9ea4b1c89111b2a680a6c0
muit <miguel_3c@hotmail.com>
2014-08-31 21:15:36 +0200

    Server functionality - Added players, Updates, etc

--- source/core/ws_server.js
+++ source/core/ws_server.js
@@ -31,27 +31,81 @@ WsServer.packets = {
 WsServer.prototype.players = [];
 WsServer.prototype.newPlayer = function(socket)
 {
-    this.players.push(socket);
-    //Create Events for all packets
-    for(var opcode in WsServer.packets)
-    {
-        //Get function name from opcode
-        var callback = WsServer.prototype[WsServer.packets[opcode]];
-        
-        //Create Event
-        this.io.on(opcode, function(data){
-            this.workingPlayer = socket;
-            callback(data);
-        });
+    socket.on("login", function(name, password){
+        var player = new Player(name, socket, 0, 0, 0);
+        this.players.push(player);
+
+        //Create Events for all packets
+        for(var opcode in WsServer.packets)
+        {
+            //Get function name from opcode
+            var callback = WsServer.prototype[WsServer.packets[opcode]];
+            
+            //Create Event
+            socket.on(opcode, function(data){
+                callback(player, data);
+            });
+        }
+    });
+}
+
+//*******************************
+//Packet Callbacks
+//*******************************
+WsServer.prototype.setDirection = function(player, direction){
+    player.direction = direction;
+}
+
+WsServer.prototype.disconect = function(player, data){
+    this.players.remove(player);
+    io.emit('players', this.players);
+}
+
+//*******************************
+// Periodic Update
+// Should be initialiced on a thread
+//*******************************
+WsServer.prototype.update = function(){
+    this.players.forEach(function(player, index, array) {
+        player.update();
     }
+
+    //Here needs a FPS controller
+
+    this.update();
 }
 
-WsServer.prototype.setDirection = function(data){
+WsServer.prototype.sendInfo = function(){
+    io.emit('info', this.players.map(function(player){
+        info = player.position;
+        info.name = player.name;
+        return info;
+    }));
+}
 
+//*******************************
+// Player class
+//*******************************
+WsServer.Player = function(name, socket, x, y, rad){
+    if(!x) x = 0;
+    if(!y) y = 0;
+    if(!rad) rad = 0;
+
+    this.name = name;
+    this.socket = socket;
+    this.position = {x: x, y: y};
+    this.direction = rad;
 }
-WsServer.prototype.disconect = function(data){
-    this.players.remove(this.workingPlayer);
+
+WsServer.Player.prototype.setPosition = function(x, y){
+    this.position = {x: x, y: y};
 }
+
+WsServer.Player.prototype.update = function(){
+    //Update position with direction
+}
+
+
 
 
 
